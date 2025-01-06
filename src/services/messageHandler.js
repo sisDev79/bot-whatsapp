878ab8e32d066c0a2637747452e343d874a0973f
Misael-GC <misael.gomez1623@gmail.com>
2025-01-05 22:27:39 -0600

    flujo para agendar una cita desde la API de WhatsApp, hay un error hay que buscarlo

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -1,5 +1,10 @@
+import { response } from 'express';
 import whatsappService from './whatsappService.js';
 class MessageHandler {
+
+  constructor(){
+    this.appointmentState = {};
+  }
   async handleIncomingMessage(message, senderInfo) {
     const fromNumber = message.from.slice(0, 2) + message.from.slice(3);
     
@@ -61,7 +66,8 @@ class MessageHandler {
     let response;
     switch (option.normalize("NFD").replace(/[\u0300-\u036f]/g, "")) {
       case 'agendar':
-        response = "Te hemos enviado un mensaje con los detalles de tu cita.";
+        this.appointmentState[to] = {step: 'name'}
+        response = "Por favor ingresa tu nombre";
         break;
       case 'consultar':
         response = "Te hemos enviado un mensaje con tus consultas.";
@@ -94,6 +100,34 @@ class MessageHandler {
     // const type = 'document';
 
     await whatsappService.sendMediaMessage(to, type, mediaUrl, caption);
+  }
+
+  async handaleAppointmentFlow(to, message){
+    const state = this.appointmentState[to];
+    let response;
+
+    switch (state.step) {
+      case 'name':
+        state.name = message;
+        state.step = 'petName';
+        response = "Gracias, Ahora, ¿Cuál es el nombre de tu Mascota?"
+        break;
+      case 'petName':
+        state.petName = message;
+        state.step = 'petType';
+        response = '¿Qué tipo de mascota es? (por ejemplo: perro, gato, huron, etc.)'
+        break;
+      case 'petType':
+        state.petType = message;
+        state.step = 'reason';
+        response = '¿Cuál es el motivo de la Consulta?';
+        break;
+      case 'reason':
+        state.reason = message;
+        response = this.completeAppointment(to);
+        break;
+    };
+    await whatsappService.sendMessage(to, response);
   }
 }
 export default new MessageHandler();
